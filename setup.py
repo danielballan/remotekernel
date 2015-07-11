54bcc746e8afd875283efc96209b9300b94d8f78
danielballan <daniel.b.allan@gmail.com>
2015-07-11 11:16:36 -0500

    initial commit -- works like normal KernelManager

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='remotekernel',
+      version='0.1.0',
+      author='Brookhaven National Laboratory',
+      packages=['remotekernel'],
+      description='A custom KernelManager for launching remote IPython kernels',
+      url='http://github.com/danielballan/remotekernel',
+      platforms='Cross platform (Linux, Mac OSX, Windows)',
+      )

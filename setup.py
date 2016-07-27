e7214949bc14ae274516accee7aa72e24ccd4c48
danielballan <daniel.b.allan@gmail.com>
2016-07-27 14:20:57 -0400

    REL: v0.2.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='remotekernel',
-      version='0.1.0',
+      version='0.2.0',
       author='Brookhaven National Laboratory',
       packages=['remotekernel'],
       description='A custom KernelManager for launching remote IPython kernels',
